203838d0889070ff617ad1718081cde0c15e0193
AndrogyChris <christopher@nikann.co.za>
2021-01-07 17:46:41 +0200

    Pneguin Jumpies

--- PenguWoosh/Assets/Scripts/PlayerController.cs
+++ PenguWoosh/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,31 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float playerSpeed = 100f;
 
+    [Header("Jump")]
+
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float fallMultiplier = 2f;
+
+
     [Header("References")]
 
     [SerializeField] Rigidbody2D rb;
+    
+    private bool facingRight = true;
 
 
     public float playerInput;
-    private bool facingRight = true;
+    public bool grounded;
+    
 
     private void Update()
     {
         playerInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            Jump();
+        }
 
     }
 
@@ -37,11 +51,21 @@ public class PlayerController : MonoBehaviour
                 FlipSprite();
             }
         }
+
+        if (!grounded && rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics.gravity.y * fallMultiplier * Time.fixedDeltaTime;
+        }
     }
 
     private void MovePlayer()
     {
         rb.velocity = new Vector2(playerInput * playerSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void FlipSprite()
@@ -50,5 +74,15 @@ public class PlayerController : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        grounded = false;
     }
 }
